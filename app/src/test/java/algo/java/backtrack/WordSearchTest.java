3a72893e8075252cab9a7ec175491d4f1b51cd8b
Ankit Singhal <ankit.singhal@gmail.com>
2026-02-24 12:29:19 -0600

    added unit tests for Word search

--- app/src/test/java/algo/java/backtrack/WordSearchTest.java
+++ app/src/test/java/algo/java/backtrack/WordSearchTest.java
@@ -10,45 +10,29 @@ import org.junit.jupiter.api.Test;
 import algo.java.utils.ArrayUtils;
 
 public class WordSearchTest {
-    WordSearch sut = new WordSearch();
-
-    @Test void wordExists_true() {
-        char[][] board = {
+    private final char[][] board = {
             {'a','b','c','d'},
             {'a','x','z','d'},
             {'a','a','c','d'},
             {'a','b','c','d'},
-        };
+    };
 
+    private final WordSearch sut = new WordSearch();
+
+    @Test void wordExists_true() {
         ArrayUtils.printArray(board);
-
         assertTrue(sut.exists(board, "abczxa"));
     }  
     
     @Test void wordExists_false() {
-        char[][] board = {
-            {'a','b','c','d'},
-            {'a','x','z','d'},
-            {'a','a','c','d'},
-            {'a','b','c','d'},
-        };
-
         ArrayUtils.printArray(board);
 
         assertFalse(sut.exists(board, "abcfxa"));
-    }   
-    
+    }
     
     @Test void searchSingleWord_success() {
-        char[][] board = {
-            {'a','b','c','d'},
-            {'a','x','z','d'},
-            {'a','a','c','d'},
-            {'a','b','c','d'},
-        };
-
-        List<String> words = List.of("abczxa");
         ArrayUtils.printArray(board);
+        List<String> words = List.of("abczxa");
 
         List<String> foundWords = sut.searchWords(board, words);
 
@@ -57,20 +41,22 @@ public class WordSearchTest {
     } 
     
     @Test void searchMultipleWord_success() {
-        char[][] board = {
-            {'a','b','c','d'},
-            {'a','x','z','d'},
-            {'a','a','c','d'},
-            {'a','b','c','d'},
-        };
-
-        List<String> words = List.of("abczxa", "dcba");
         ArrayUtils.printArray(board);
+        List<String> words = List.of("abczxa", "dcba");
 
         List<String> foundWords = sut.searchWords(board, words);
 
         assertEquals(2, foundWords.size());
         assertTrue(foundWords.contains(words.get(0)));
         assertTrue(foundWords.contains(words.get(1)));
-    } 
+    }
+
+    @Test void searchMultipleWord_notFound() {
+        ArrayUtils.printArray(board);
+        List<String> words = List.of("abdzxa", "dssa");
+
+        List<String> foundWords = sut.searchWords(board, words);
+
+        assertEquals(0, foundWords.size());
+    }
 }
